4dbd962ee21b52cd20231556d206645e859bf2d4
QuicksandDesignStudio <romit@quicksand.co.in>
2020-03-05 18:15:36 +0530

    Adding Cors

--- src/api-app/app/main/start.py
+++ src/api-app/app/main/start.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_restful import Resource, Api
+from flask_cors import CORS
 from .config import config_by_name
 
 db = SQLAlchemy()
@@ -11,27 +12,32 @@ app = None
 
 
 def create_app(config_name):
-	app = Flask(__name__)
-	app.config.from_object(config_by_name[config_name])
-	db.init_app(app)
-	flask_bcrypt.init_app(app)
-	api = Api(app)	
-	
-	#all the API end points setup
-	from .controller import sample_api
-	from .controller import category_api
-	from .controller import classification_api
-	from .controller import download
-	api.add_resource(sample_api.Sample, '/api/sample/<string:sample_id>')
-	api.add_resource(sample_api.SampleList, '/api/samples')
-	api.add_resource(download.SampleDownload, '/api/download/<string:file_name>')
-	api.add_resource(category_api.Category, '/api/category/<string:category_id>')
-	api.add_resource(category_api.CategoryList, '/api/categories')
-	api.add_resource(classification_api.Classification, '/api/classification/<string:classification_id>')
-	api.add_resource(classification_api.ClassificationList, '/api/classifications')
+    app = Flask(__name__)
+    CORS(app)
+    app.config.from_object(config_by_name[config_name])
+    db.init_app(app)
+    flask_bcrypt.init_app(app)
+    api = Api(app)
 
-	#this is where the regular web gets started
-	app.app_context().push()
-	from app.main.controller import application
+    # all the API end points setup
+    from .controller import sample_api
+    from .controller import category_api
+    from .controller import classification_api
+    from .controller import download
+    api.add_resource(sample_api.Sample, '/api/sample/<string:sample_id>')
+    api.add_resource(sample_api.SampleList, '/api/samples')
+    api.add_resource(download.SampleDownload,
+                     '/api/download/<string:file_name>')
+    api.add_resource(category_api.Category,
+                     '/api/category/<string:category_id>')
+    api.add_resource(category_api.CategoryList, '/api/categories')
+    api.add_resource(classification_api.Classification,
+                     '/api/classification/<string:classification_id>')
+    api.add_resource(classification_api.ClassificationList,
+                     '/api/classifications')
 
-	return app
+    # this is where the regular web gets started
+    app.app_context().push()
+    from app.main.controller import application
+
+    return app
